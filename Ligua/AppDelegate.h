87caa3f8530fdc2ce8064f6c6de2c25ba7eb9c61
FrancescoZ <francesco.zanoli@gmail.com>
2015-11-25 11:50:00 +0100

    First commit

---
+++ Ligua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ligua
+//
+//  Created by Francesco Zanoli on 25/11/15.
+//  Copyright © 2015 Francesco Zanoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ligua/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Ligua
+//
+//  Created by Francesco Zanoli on 25/11/15.
+//  Copyright © 2015 Francesco Zanoli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
