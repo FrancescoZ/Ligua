4a5fe95977125e2d4e35fee446b9f34c49b958ab
FrancescoZ <francesco.zanoli@gmail.com>
2015-11-25 12:13:59 +0100

    Creation Domain.Group Creation Domain.User

---
+++ Group.m
@@ -0,0 +1,14 @@
+//
+//  Group.m
+//  Ligua
+//
+//  Created by Francesco Zanoli on 25/11/15.
+//  Copyright © 2015 Francesco Zanoli. All rights reserved.
+//
+
+#import "Group.h"
+
+@implementation Group
+
+
+@end
